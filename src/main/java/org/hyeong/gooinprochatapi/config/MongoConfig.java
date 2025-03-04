d4475bab86ef7dfe61b4afbc6532365e57f2de14
jhyeong <hyeongcode@gmail.com>
2025-03-04 14:21:00 +0900

    clore: mongo config mongourl edit

--- src/main/java/org/hyeong/gooinprochatapi/config/MongoConfig.java
+++ src/main/java/org/hyeong/gooinprochatapi/config/MongoConfig.java
@@ -2,6 +2,7 @@ package org.hyeong.gooinprochatapi.config;
 
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.core.MongoTemplate;
@@ -9,10 +10,11 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 @Configuration
 public class MongoConfig {
 
+    @Value("${spring.data.mongodb.uri}")
+    private String mongoUri;
+
     @Bean
     public MongoTemplate mongoTemplate() {
-
-        String mongoUri = "mongodb://gooinprochatdbuser:gooinprochatdbuser@localhost:27017/gooinprochatdb?authSource=gooinprochatdb";
 
         MongoClient mongoClient = MongoClients.create(mongoUri);
         return new MongoTemplate(mongoClient, "gooinprochatdb");
